7409abd9a12f104be8e083ef7e466bed9ee1862c
Donald Abrams <donabrams@gmail.com>
2011-01-31 15:31:00 -0500

    update README and clean up transforms lib

--- jquery.formObjTransforms.js
+++ jquery.formObjTransforms.js
@@ -17,25 +17,6 @@
         }
         return val;
     };
-	var addToObject = function(obj, name, value) {
-	    var pkg = name.split('.', 1)[0];
-	    if ((pkg.length + 1) < name.length) {
-	        obj[pkg] = addToObject(obj[pkg] || {}, name.substring(pkg.length + 1), value);
-	        return obj;
-	    }
-	    if (obj[name]) {
-	        if ($.isArray(obj[name])) {
-	            obj[name].push(value);
-	        }
-	        else {
-	            obj[name] = [obj[name], value];
-	        }
-	    }
-	    else {
-	        obj[name] = value;
-	    }
-        return obj;
-    };
 	var addToObjectOrArray = function(obj, name, value) {
 		var pkg;
 		var subPkg;
@@ -73,58 +54,64 @@
 		}
         return obj;
     };
-    $.fn.formToObj = function() {
-        var toRet = {};
+    $.fn.formToObj = function(toAddTo, property) {
+        var toAddTo = toAddTo || {};
+		if (property) {
+			toAddTo[property] = toAddTo[property] || {};
+			toPopulate = toAddTo[property];			
+		} else {
+			toPopulate = toAddTo;
+		}
         $('input[type=text][name],input[type=hidden][name],textarea[name]', this).each(function(i) {
-            addToObjectOrArray(toRet, this.name, parseFormVal(this.value));
+            addToObjectOrArray(toPopulate, this.name, parseFormVal(this.value));
         });
         $('input[type=checkbox][name],input[type=radio][name]', this).each(function(i) {
             if (this.checked) {
-                addToObjectOrArray(toRet, this.name, parseFormVal(this.value));
+                addToObjectOrArray(toPopulate, this.name, parseFormVal(this.value));
             }
         });
         $('select[name]', this).each(function(i) {
             var select = this;
             $("option", this).each(function(j) {
                 if (this.selected) {
-                    addToObjectOrArray(toRet, select.name, parseFormVal(this.value));
+                    addToObjectOrArray(toPopulate, select.name, parseFormVal(this.value));
                 }
             });
         });
-        return toRet;
+        return toAddTo;
     };
-    $.flattenObject = function(obj, prefix, ret, options) {
-        var ret = ret || {};
+    $.flattenObject = function(toFlatten, toAddTo, property, options) {
+        var toAddTo = toAddTo || {};
         var nullsAsEmptyString = options && options.nullsAsEmptyString;
-		if ($.isArray(obj)) {
-			for (var i = 0; i < obj.length;i++) {
-				if (undefined !== obj[i]) {
-					$.flattenObject(obj[i], prefix ? (prefix + "[" + i + "]") : ("[" + i + "]"), ret, options);
+		if ($.isArray(toFlatten)) {
+			for (var i = 0; i < toFlatten.length;i++) {
+				if (undefined !== toFlatten[i]) {
+					$.flattenObject(toFlatten[i], property ? (property + "[" + i + "]") : ("[" + i + "]"), toAddTo, options);
 				}
 			}
 		}
-		else if ((typeof obj == 'object') && (null !== obj)) {
-            $.each(obj, function(i) {
-				if (undefined !== obj[i]) {
-					$.flattenObject(obj[i], prefix ? (prefix + "." + i) : i, ret, options);
+		else if ((typeof toFlatten == 'object') && (null !== toFlatten)) {
+            $.each(toFlatten, function(i) {
+				if (undefined !== toFlatten[i]) {
+					$.flattenObject(toFlatten[i], property ? (property + "." + i) : i, toAddTo, options);
 				}
             });
 		}
 		else {
-			if (prefix) {
-				ret[prefix] = nullsAsEmptyString ? (obj == null ? "" : String(obj)) : String(obj);
+			if (property) {
+				toAddTo[property] = nullsAsEmptyString ? (toFlatten == null ? "" : String(toFlatten)) : String(toFlatten);
 			}
 			else {
-				ret = nullsAsEmptyString ? (obj == null ? "" : String(obj)) : String(obj);
+				toAddTo = nullsAsEmptyString ? (toFlatten == null ? "" : String(toFlatten)) : String(toFlatten);
 			}
 		}
-		return ret;
+		return toAddTo;
     };
     $.fn.objToForm = function(obj, clearFirst) {
         var fdoc = $.flattenObject(obj);
         var that = $(this);
         if (clearFirst) {
-            that.clearForm();
+            that.clearFields();
         }
 		var arrays = {};
         $.each(fdoc, function(i) {
@@ -140,12 +127,12 @@
             that.children('input[name="' + i + '"],textarea[name="' + i + '"],select[name="' + i + '"]').val(arrays[i]);
         });
     };
-    $.fn.clearForm = function() {
+    $.fn.clearFields = $.fn.clearInputs = function() {
         return this.each(function() {
-            $('input,select,textarea', this).clearFields();
+            $('input,select,textarea', this).clearField();
         });
     };
-    $.fn.clearFields = $.fn.clearInputs = function() {
+    $.fn.clearField = $.fn.clearInput = function() {
         return this.each(function() {
             var t = this.type,
                 tag = this.tagName.toLowerCase();
